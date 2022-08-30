e63376f121ed007d93c98e21d8c39f1359520432
J0rdyV <28193980+J0rdyV@users.noreply.github.com>
2022-08-30 12:21:54 +0200

    updates

--- blocks.h
+++ blocks.h
@@ -1,14 +1,42 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "echo ' ' $(cat /proc/loadavg | awk '{ print $2 }') - \
-		$(sensors | awk '/Core 0/ { print $3 }' | sed s/+//g) - \
-		$(free -h | awk '/^Mem/ { print $3 }' | sed s/i//g)", 5, 0},
-	{"", "echo $(cat /sys/class/power_supply/BAT0/capacity)%", 60, 0},
-	{"", "date '+%d/%m %H:%M'", 5, 0},
-	{"", "echo \"$(whoami) \"", 100, 0},
+	/*
+		$(sensors | awk '/edge/ { print $2 }' | sed s/+//g) \
+	*/
+	{"", "[ -f ~/.config/nvim/vimwiki/diary/$(date '+%Y-%m-%d').wiki ] || \
+		echo '📓Wiki entry not created today'", 60, 0},
+
+	{" ", "echo 🔭$(cat /proc/loadavg | awk '{ print $1 }') \
+		$(free -h | awk '/^Mem/ { print $3 }' | sed s/i//g)", 3, 0},
+													   //
+	{"", "echo -n $(newsboat -x print-unread | awk '{ if($1>0) print \"📰\" $1 }')", 60, 0},
+
+	{"", "echo -n $(find \"${XDG_DATA_HOME:-$HOME/.local/share}\"/mail/*/INBOX/new/* -type f | \
+		wc -l 2>/dev/null | awk '{ if($1>0) print \"📬\" $1 }')", 60, 0},
+
+	/* {"🔋", "echo -n $(cat /sys/class/power_supply/BAT0/capacity)%", 60, 0}, */
+	{"", "echo -n $(if [ $(cat /sys/class/power_supply/BAT0/status) == 'Charging' ]; then echo ⚡; else echo 🔋; fi)$(cat /sys/class/power_supply/BAT0/capacity)", 60, 0},
+
+	/* {"", "curl -s 'wttr.in/Lokeren?format=%c%t' | sed 's/ +//g'", 3600, 0}, */
+	{"", "curl -s 'wttr.in/Lokeren?format=%c' | sed 's/ //g'", 900, 0},
+
+	/* {"", "echo \"$(curl 'wttr.in/Lokeren?format=%m%M')/28\"", 21600, 0}, */
+	{"", "curl -s 'wttr.in/Lokeren?format=%m'", 21600, 0},
+
+	/* {"", "echo ⏰$(date '+%d/%m %H:%M')", 5, 0}, */
+	{"⏰", "date '+%H:%M'", 5, 0},
+
+	{"", "if $(dunstctl is-paused); then echo '🔕'; fi", 5, 0},
+
+	{"", "echo -n $(if [ \"$(nmcli device status | grep wifi | awk '{ print $3 }')\" == 'connected' ]; then echo 📡; fi)", 5, 0},
+
+	{"", "echo -n $(if [ \"$(nmcli device status | grep vpn0 | awk '{ print $3 }')\" == 'connected' ]; then echo 🔒; fi)", 5, 0},
+
+	{"", "chars=🌌🦉🧙🦂 && echo -n \"${chars:RANDOM%${#chars}:1}$(whoami)\"", 3600, 0},
+
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = " ";
 static unsigned int delimLen = 5;
